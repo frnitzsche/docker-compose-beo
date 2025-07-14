6d0f3526310a8e5ae4e90def4e561451473ffc0e
Friederich Nietzsche <frnitzsche@yahoo.ca>
2025-07-14 10:04:51 +0300

    added customization scripts

---
+++ aws-ec2-instance-user-data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo yum update -y && \
+sudo yum install git -y && \
+git clone https://github.com/frnitzsche/docker-compose-beo.git && \
+cd docker-compose-beo && \
+chmod a+x customization-script.sh && \
+/bin/bash customization-script.sh
+
+
+
+
+
+
