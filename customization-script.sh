3ceba1bbd916a1c72672b62a54b7814940fe0e05
Friederich Nietzsche <frnitzsche@yahoo.ca>
2025-07-14 18:30:29 +0300

    aa

--- customization-script.sh
+++ customization-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 host="beo-anishev.mywire.org"
-sudo yum install docker -y && \
+sudo yum install docker nginx certbot certbot-nginx -y && \
 sudo systemctl enable docker.service && \
 sudo systemctl start docker.service && \
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-linux-$(uname -m) -o /usr/bin/docker-compose && \
@@ -12,7 +12,6 @@ sudo yum install ./dynu-ip-update-client_1.0.2-1_amd64.rpm -y && \
 sudo cp /docker-compose-beo/appsettings.json /usr/share/dynu-ip-update-client/appsettings.json && \
 sudo systemctl restart dynu-ip-update-client.service && \
 sleep 5s && \
-sudo yum install nginx certbot certbot-nginx -y && \
 sudo sed -i "s/server_name  _;/server_name ${host};/g" /etc/nginx/nginx.conf && \
 
 sed '/root         \/usr\/share\/nginx\/html;/r'<(cat <<EOF
