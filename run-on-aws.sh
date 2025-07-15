ad5f557422a3e49d7fbd7015229c64b88c5c4b14
Friederich Nietzsche <frnitzsche@yahoo.ca>
2025-07-15 11:24:21 +0300

    a

--- run-on-aws.sh
+++ run-on-aws.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws ec2 run-instances --region eu-central-1 --image-id ami-0af9b40b1a16fe700 --instance-type t2.micro --user-data file://user-data.sh
+aws ec2 run-instances --region eu-central-1 --image-id ami-0af9b40b1a16fe700 --instance-type t2.micro --block-device-mappings DeviceName=/dev/xvda,Ebs={VolumeSize=30} --user-data file://user-data.sh
