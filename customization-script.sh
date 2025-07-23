464a074780bfac3375c4d753c84d7c6fad9caebb
Friederich Nietzsche <frnitzsche@yahoo.ca>
2025-07-23 19:46:50 +0300

    a

--- customization-script.sh
+++ customization-script.sh
@@ -2,19 +2,35 @@
 host="beo-anishev.mywire.org"
 dir="/docker-compose-beo"
 
-curl "http://api.dynu.com/nic/update?hostname=$host&myip=$(curl -s ifconfig.me)&password=faf0152cfacc4704af98927ae6dd55f4"
+echo ">>>>> Updating DNS entry for host $host" && \
+curl "http://api.dynu.com/nic/update?hostname=$host&myip=$(curl -s ifconfig.me)&password=faf0152cfacc4704af98927ae6dd55f4"  && \
+
+echo '>>>>> Installing docker' && \
 sudo yum install docker -y && \
+
+echo '>>>>> Strarting docker service' && \
 sudo systemctl enable docker.service && \
 sudo systemctl start docker.service && \
+
+echo '>>>>> Installing docker-compose' && \
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-linux-$(uname -m) -o /usr/bin/docker-compose && \
 sudo chmod 755 /usr/bin/docker-compose && \
 cd /$dir && \
+
+echo '>>>>> Running docker-compose' && \
 sudo docker-compose up -d && \
 
+echo '>>>>> Sleeping for 5 sec' && \
 sleep 5s && \
+
+echo '>>>>> Installing nginx certbot certbot-nginx' && \
 sudo yum install nginx certbot certbot-nginx -y && \
-sudo sed -i "s/server_name  _;/server_name ${host};/g" /etc/nginx/nginx.conf && \
+
+echo '>>>>> Setting password for user ec2-user' && \
 echo 'ec2-user:478312zxc' | sudo chpasswd && \
+
+echo '>>>>> Modifying nginx.conf' && \
+sudo sed -i "s/server_name  _;/server_name ${host};/g" /etc/nginx/nginx.conf && \
 
 sed '/root         \/usr\/share\/nginx\/html;/r'<(cat <<EOF
         location / {
@@ -27,22 +43,27 @@ sed '/root         \/usr\/share\/nginx\/html;/r'<(cat <<EOF
 EOF
 ) -i -- /etc/nginx/nginx.conf && \
 
+echo '>>>>> Restarting nginx server' && \
 sudo systemctl enable nginx.service && \
 sudo systemctl restart nginx.service && \
-sleep 60s && \
-sudo certbot --nginx -d $host -m my@mail.com --agree-tos -n --test-cert
 
-echo '>>>>> Downloading beodb sql dump file from S3 service'
-sudo aws s3 cp s3://beo-anishev/beodb.bz2 /$dir
+echo '>>>>> Sleeping for 60 sec' && \
+sleep 60s && && \
 
-echo '>>>>> Uncompressing beodb sql dump file'
+echo '>>>>> Running certbot' && \
+sudo certbot --nginx -d $host -m my@mail.com --agree-tos -n --test-cert && \
+
+echo '>>>>> Downloading beodb sql dump file from S3 service' && \
+sudo aws s3 cp s3://beo-anishev/beodb.bz2 /$dir && \
+
+echo '>>>>> Uncompressing beodb sql dump file' && \
 sudo bzip2 -d beodb.bz2 && \
 
-echo '>>>>> Installing MariaDB client tools'
+echo '>>>>> Installing MariaDB client tools' && \
 sudo yum install mariadb1011-client-utils -y && \
 
-echo '>>>>> Installing Pipe Viewer'
-sudo yum install pv -y
+echo '>>>>> Installing Pipe Viewer' && \
+sudo yum install pv -y && \
 
-echo '>>>>> Importing beodb sql dump file'
-pv /$dir/beodb | mysql -h 127.0.0.1 -u myuser -p mypassword moussala
+# echo '>>>>> Importing beodb sql dump file' && \
+# pv -c /$dir/beodb | mysql -h 127.0.0.1 -u myuser -p mypassword moussala
